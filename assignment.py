2d548d0832152bde33563cac7b3071c59fd62269
batorgiln2016-sketch <batorgil.n2016@orkhonschool.edu.mn>
2026-09-21 09:12:34 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,16 +1,12 @@
 # You can remove 'pass' if you written code in the function 
-
 # Exercise 1
 def count_characters(text):
-    # Write your code here
-    pass
+    print(len(text))
+
 
 # Exercise 2
 def remove_spaces(text):
-    # Write your code here
-    pass
-
-# Exercise 3
+    print(text.replace(" ", ""))# Exercise 3
 def count_vowels(text):
     # Write your code here
     pass
